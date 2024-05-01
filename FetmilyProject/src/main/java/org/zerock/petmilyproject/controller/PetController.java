97d122f12ff2a18b9640a7958a69d81d4dce245f
dyddks <dyddks0115@naver.com>
2024-05-01 16:44:24 +0900

    feat: 반려동물 crud 구현

--- FetmilyProject/src/main/java/org/zerock/petmilyproject/controller/PetController.java
+++ FetmilyProject/src/main/java/org/zerock/petmilyproject/controller/PetController.java
@@ -1,4 +1,42 @@
 package org.zerock.petmilyproject.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.petmilyproject.dto.PetDTO;
+import org.zerock.petmilyproject.service.PetService;
+
+import java.util.List;
+
+@Log4j2
+@Controller
+@RequestMapping("/pet")
+@RequiredArgsConstructor
 public class PetController {
+    private final PetService petService;
+
+    @GetMapping("/list")
+    public void petList(@RequestParam("memberId") Long memberId, Model model){
+        List<PetDTO> petlist = petService.petList(memberId);
+        model.addAttribute("petList", petlist);
+    }
+
+    @PostMapping(value = "/insert", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String petInsert(@RequestBody PetDTO petDTO, RedirectAttributes redirectAttributes){
+        petService.register(petDTO);
+        redirectAttributes.addAttribute("memberId", petDTO.getMemberId());
+        return "redirect:/pet/list";
+    }
+
+    @PostMapping(value = "/update", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String petUpdate(@RequestBody PetDTO petDTO, RedirectAttributes redirectAttributes){
+        petService.modify(petDTO);
+
+        return "redirect:/pet/read";
+    }
+
 }
---
+++ FetmilyProject/src/main/java/org/zerock/petmilyproject/repository/PetRepository.java
@@ -0,0 +1,15 @@
+package org.zerock.petmilyproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.zerock.petmilyproject.domain.Pet;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PetRepository extends JpaRepository<Pet, Long> {
+    Optional<Pet> findByPetId(Long petId);
+
+    @Query("select p from Pet p where p.member.memberId=:memberId")
+    List<Pet> searchAll(Long memberId);
+}
