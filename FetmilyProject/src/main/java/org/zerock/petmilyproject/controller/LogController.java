11a6107acb317074fe5408c73bc7f86fba32ef54
dyddks <dyddks0115@naver.com>
2024-05-01 11:09:23 +0900

    feat: 회원가입 구현

--- FetmilyProject/src/main/java/org/zerock/petmilyproject/controller/LogController.java
+++ FetmilyProject/src/main/java/org/zerock/petmilyproject/controller/LogController.java
@@ -1,4 +1,39 @@
 package org.zerock.petmilyproject.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.petmilyproject.dto.MemberDTO;
+import org.zerock.petmilyproject.service.LogService;
+
+@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
 public class LogController {
+    private final LogService logService;
+
+    @GetMapping("/signup")
+    public void signupGET(){}
+
+    @PostMapping(value = "/signup", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String signup(@RequestBody MemberDTO memberDTO){
+        logService.register(memberDTO);
+
+        return "redirect:";
+    }
+
+    @GetMapping("/login")
+    public void loginGET(){}
+
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String login(@RequestBody MemberDTO memberDTO){
+        MemberDTO loginMemberDTO = logService.login(memberDTO);
+        System.out.println(loginMemberDTO);
+        return "redirect:";
+    }
+
 }
--- FetmilyProject/src/main/java/org/zerock/petmilyproject/service/LogServiceImpl.java
+++ FetmilyProject/src/main/java/org/zerock/petmilyproject/service/LogServiceImpl.java
@@ -1,11 +1,31 @@
 package org.zerock.petmilyproject.service;
 
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.zerock.petmilyproject.domain.Member;
 import org.zerock.petmilyproject.dto.MemberDTO;
+import org.zerock.petmilyproject.repository.LogRepository;
 
+@Service
+@RequiredArgsConstructor
 public class LogServiceImpl implements LogService{
+    private final ModelMapper modelMapper;
+    private final LogRepository logRepository;
     @Override
     public Long register(MemberDTO memberDTO) {
-        return null;
+        Member member = modelMapper.map(memberDTO, Member.class);
+        Long memberId = logRepository.save(member).getMemberId();
+
+        return memberId;
+    }
+
+    @Override
+    public MemberDTO login(MemberDTO memberDTO){
+        Member member = modelMapper.map(memberDTO, Member.class);
+        Member loginMember = logRepository.login(member.getEmail(), member.getPassword());
+
+        return modelMapper.map(loginMember, MemberDTO.class);
     }
 
     @Override
