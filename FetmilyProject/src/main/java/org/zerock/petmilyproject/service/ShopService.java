ef4332af89f0cd5c98a428326b05ae42c7afa291
dyddks <dyddks0115@naver.com>
2024-05-03 17:57:11 +0900

    feat: 상품리스트가져오기 구현

--- FetmilyProject/src/main/java/org/zerock/petmilyproject/service/ShopService.java
+++ FetmilyProject/src/main/java/org/zerock/petmilyproject/service/ShopService.java
@@ -1,6 +1,12 @@
 package org.zerock.petmilyproject.service;
 
+import org.springframework.data.domain.Page;
 import org.zerock.petmilyproject.dto.ItemDTO;
+import org.zerock.petmilyproject.dto.PageRequestDTO;
+import org.zerock.petmilyproject.dto.PageResponseDTO;
+import org.zerock.petmilyproject.dto.ReplyDTO;
+
+import java.util.List;
 
 public interface ShopService {
 
@@ -8,4 +14,5 @@ public interface ShopService {
     ItemDTO readOne(Long bno);
     void modify(ItemDTO itemDTO);
     void remove(Long bno);
+    PageResponseDTO<ItemDTO> getListOfItem(PageRequestDTO pageRequestDTO);
 }
--- FetmilyProject/src/main/java/org/zerock/petmilyproject/service/ShopServiceImpl.java
+++ FetmilyProject/src/main/java/org/zerock/petmilyproject/service/ShopServiceImpl.java
@@ -1,8 +1,28 @@
 package org.zerock.petmilyproject.service;
 
-import org.zerock.petmilyproject.dto.ItemDTO;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.zerock.petmilyproject.domain.Item;
+import org.zerock.petmilyproject.dto.*;
+import org.zerock.petmilyproject.repository.ShopRepository;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class ShopServiceImpl implements ShopService {
+    private final ShopRepository shopRepository;
+    private final ModelMapper modelMapper;
+
     @Override
     public Long register(ItemDTO itemDTO) {
         return null;
@@ -21,5 +41,23 @@ public class ShopServiceImpl implements ShopService {
     @Override
     public void remove(Long bno) {
 
+    }
+
+    @Override
+    public PageResponseDTO<ItemDTO> getListOfItem(PageRequestDTO pageRequestDTO) {
+        Pageable pageable = PageRequest.of(pageRequestDTO.getPage() <=0? 0: pageRequestDTO.getPage() -1,
+                pageRequestDTO.getSize(),
+                Sort.by("itemId").ascending());
+
+        Page<Item> result = shopRepository.listOfItem(pageable);
+
+        List<ItemDTO> dtoList = result.getContent().stream().map(item -> modelMapper.map(item, ItemDTO.class))
+                    .collect(Collectors.toList());
+
+        return PageResponseDTO.<ItemDTO>withAll()
+                .pageRequestDTO(pageRequestDTO)
+                .dtoList(dtoList)
+                .total((int)result.getTotalElements())
+                .build();
     }
 }
