679947c292b49182aab313dc74919143aa3e3c12
yamji <117097672+yamji@users.noreply.github.com>
2024-05-07 12:47:49 +0900

    일정 등록 백엔드

--- FetmilyProject/src/main/java/org/zerock/petmilyproject/controller/CalendarController.java
+++ FetmilyProject/src/main/java/org/zerock/petmilyproject/controller/CalendarController.java
@@ -1,4 +1,29 @@
 package org.zerock.petmilyproject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.petmilyproject.dto.EventDTO;
+import org.zerock.petmilyproject.service.EventService;
+
+@RestController
+@RequestMapping("/schedules")
 public class CalendarController {
+
+    @Autowired
+    private EventService EventService;
+
+    @PostMapping
+    public ResponseEntity<String> addSchedule(@RequestBody EventDTO eventDTO) {
+        try {
+            EventService.addEvent(eventDTO); // 서비스로 DTO 전달하여 스케줄 추가
+            return ResponseEntity.ok("Schedule added successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to add schedule");
+        }
+    }
 }
---
+++ FetmilyProject/src/main/java/org/zerock/petmilyproject/service/EventService.java
@@ -0,0 +1,7 @@
+package org.zerock.petmilyproject.service;
+
+import org.zerock.petmilyproject.dto.EventDTO;
+
+public interface EventService {
+    void addEvent(EventDTO eventDTO);
+}
