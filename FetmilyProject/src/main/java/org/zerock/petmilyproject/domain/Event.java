c061ce65680bec426e7221cb3ade6ba1d343d7cc
yamji <117097672+yamji@users.noreply.github.com>
2024-05-07 17:49:39 +0900

    Event.java 등록

--- FetmilyProject/src/main/java/org/zerock/petmilyproject/domain/Event.java
+++ FetmilyProject/src/main/java/org/zerock/petmilyproject/domain/Event.java
@@ -29,4 +29,19 @@ public class Event extends BaseEntity {
     private LocalDateTime endDate;
 
     private Long cycle;
+
+    public void setContent(String content) {
+    }
+
+    public void setEventColor(String eventColor) {
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+    }
+
+    public void setCycle(Long cycle) {
+    }
 }
