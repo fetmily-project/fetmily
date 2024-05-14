e075d250e63f823fab95c03b645ca59be54759f8
NCP08 <navercloudplatform08.gmail.com>
2024-05-14 16:57:36 +0900

    banner 수정

--- FetmilyProject/src/main/java/org/zerock/petmilyproject/controller/mypage/BuyController.java
+++ FetmilyProject/src/main/java/org/zerock/petmilyproject/controller/mypage/BuyController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.zerock.petmilyproject.service.ShopService;
+//import org.zerock.petmilyproject.service.ShopService;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/mypage/buy")
 @RequiredArgsConstructor
 public class BuyController {
-    ShopService shopService;
+//    ShopService shopService;
 
     @GetMapping("/list")
     public void orderListGET(Model model, HttpServletRequest httpServletRequest){
--- FetmilyProject/src/main/java/org/zerock/petmilyproject/service/member/LogService.java
+++ FetmilyProject/src/main/java/org/zerock/petmilyproject/service/member/LogService.java
@@ -10,7 +10,7 @@ public interface LogService {
     MemberDTO readOne(Long memberId);
     MemberDTO login(MemberDTO memberDTO);
 
-    void modify(MemberDTO memberDTO);
+//    void modify(MemberDTO memberDTO);
 
 
 
